394e781e2f38af419a92800ec7cdec954bb80e48
saeed-hunain <hunain.saeed@gomotive.com>
2022-11-18 11:53:53 +0500

    added flesh messages in main only

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,7 @@
 class MainController < ApplicationController
     def index
-        
+        flash.now[:notice] = "Hello I am notice"
+        flash.now[:alert] = "Hello I am alert"
     end
     
 end
