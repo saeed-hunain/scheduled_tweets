bbf6ee8f163b06dbacdb9187082708e8f25d8ec5
saeed-hunain <hunain.saeed@gomotive.com>
2022-11-18 20:05:17 +0500

    sign in functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,17 @@ class SessionsController < ApplicationController
     def new
     end
 
+    # This method will create user session if credentials are correct
     def create
+        user = User.find_by(email: params[:email])
+
+        if user.present? && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to root_path, notice: "Logged in successfully"
+        else
+            flash.now[:alert] = "Invalid email or password"
+            render :new, status: :unprocessable_entity
+        end
     end
 
     def destory
