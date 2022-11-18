5ccd1e61b52c896406641c8fa1eb6e31fe712f0b
saeed-hunain <hunain.saeed@gomotive.com>
2022-11-18 18:42:43 +0500

    user registration

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,25 @@
+class RegistrationsController < ApplicationController
+    def new
+        @user = User.new
+    end
+    
+    def create
+        @user = User.new(user_params)
+        
+        if @user.save
+            redirect_to root_path, notice: "Successfully created account"
+        else
+            # flash[:alert] = "Something was wrong"
+            render :new
+        end
+    end
+
+    private
+    def user_params
+        # same as params[:user] but .required will raise error if user is empty
+        params.required(:user).permit(:email, :password, :password_confirmation)
+
+
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get "about", to: 'about#index'
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
 
   root to: "main#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
