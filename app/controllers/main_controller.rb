2cc85a2eebdaa5489793a92070b40ef46df0c30c
saeed-hunain <hunain.saeed@gomotive.com>
2022-11-19 13:00:16 +0500

    Current state and sign in & up btn created

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,9 +2,7 @@ class MainController < ApplicationController
     def index
         # flash.now[:notice] = "Hello I am notice"
         # flash.now[:alert] = "Hello I am alert"
-        if session[:user_id]
-            @user = User.find_by(id: session[:user_id])
-        end
+        
     end
     
 end
